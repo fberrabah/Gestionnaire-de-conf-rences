fa62963576ac7647c9935719003657c285718f7a
faridz <farid.berrabah@gmail.com>
2020-02-13 12:12:25 +0100

    add datetime

--- controller/conf/gestionconf.py
+++ controller/conf/gestionconf.py
@@ -1,5 +1,6 @@
 from model.connection import *
 from model.entities import *
+import datetime
 
 class Createconf():
 
@@ -17,12 +18,13 @@ class Createconf():
         self.choice.initialize_connection()
         self.title= input("Entrer un titre:").lower()
         self.resume = input("Entrer un résumé :").lower()
-        self.date = input("Entrer une date :").lower()
-        self.hour = input("Entrer une heur :").lower()
+        self.date = input("Entrer une date :")
+        self.hour = input("Entrer une heur :")
         self.speak_id = input("Entrer l'ID du conférencier :")
+        actual_date = datetime.datetime.today()
         
-        self.choice.cursor.execute("INSERT INTO conf(title, resume, date, hour, speak_id) VALUES"
-                                  "(%s, %s, %s, %s, %s)" ,(self.title, self.resume, self.date , self.hour, self.speak_id))
+        self.choice.cursor.execute("INSERT INTO conf(title, resume, date, hour, speak_id, creation_date) VALUES"
+                                  "(%s, %s, %s, %s, %s, %s)" ,(self.title, self.resume, self.date , self.hour, self.speak_id, actual_date))
         self.choice.connection.commit()
         self.choice.close_connection()
         print("La conférence est enregistrée.")
